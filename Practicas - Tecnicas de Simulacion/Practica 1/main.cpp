7ba9155879594aa84c33cf423280494c0f024821
Luisa Arevalo <00038617@uca.edu.sv>
2020-03-24 16:34:45 -0600

    Practica 1 de Simulacion

---
+++ Practicas - Tecnicas de Simulacion/Practica 1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "math_tools.h"
+#include "display_tools.h"
+
+
+int main(void){
+
+    /*Vector zeroes_vector;
+    zeroes(zeroes_vector, 5);
+    showVector(zeroes_vector);*/
+
+    Matrix zeroes_matrix;
+    zeroes(zeroes_matrix, 5);
+    showMatrix(zeroes_matrix);
+
+    return 0;
+}
