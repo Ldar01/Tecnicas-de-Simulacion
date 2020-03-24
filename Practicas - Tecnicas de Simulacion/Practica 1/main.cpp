b33c82cf96ab83aa74b5178a9c235474e4c38307
Luisa Arevalo <00038617@uca.edu.sv>
2020-03-24 17:03:52 -0600

    Tarea 1 de Simulacion Finished

--- Practicas - Tecnicas de Simulacion/Practica 1/main.cpp
+++ Practicas - Tecnicas de Simulacion/Practica 1/main.cpp
@@ -9,9 +9,26 @@ int main(void){
     zeroes(zeroes_vector, 5);
     showVector(zeroes_vector);*/
 
-    Matrix zeroes_matrix;
+    /*Matrix zeroes_matrix;
     zeroes(zeroes_matrix, 5);
-    showMatrix(zeroes_matrix);
+    showMatrix(zeroes_matrix);*/
+
+    //Probando Tarea
+    Matrix matrixA, matrixInvA;
+
+    zeroes(matrixA, 3);
+    //zeroes(matrixInvA, 3);
+
+    matrixA.at(0).at(0) = 2; matrixA.at(0).at(1) = 2; matrixA.at(0).at(2) = 3;
+    matrixA.at(1).at(0) = 4; matrixA.at(1).at(1) = 5; matrixA.at(1).at(2) = 6;
+    matrixA.at(2).at(0) = 7; matrixA.at(2).at(1) = 8; matrixA.at(2).at(2) = 9;
+
+    cout<<"========= Matrix A ============\n";
+    showMatrix(matrixA);
+
+    cout<<"========= Matrix Inv A ============\n";
+    inverseMatrix(matrixA, matrixInvA);
+    showMatrix(matrixInvA);
 
     return 0;
 }
