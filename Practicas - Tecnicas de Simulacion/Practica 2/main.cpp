2c9e57cdfaba0673e0070babd07d1ff31d032712
Luisa Arevalo <00038617@uca.edu.sv>
2020-04-01 15:52:14 -0600

    Practica 2 segun video

---
+++ Practicas - Tecnicas de Simulacion/Practica 2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "math_tools.h"
+#include "display_tools.h"
+#include "classes.h"
+
+//enum materias{simu, bi, arqui=12, capas};
+
+int main(void){
+
+   //materias m = simu;
+   //cout<< m << endl; //0
+   //cout<< capas <<endl; //13
+
+    return 0;
+}
